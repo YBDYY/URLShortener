99f24a8e03b9876e8ed99037c04ecd8176edd418
n4tas <natas.mazeika@gmail.com>
2025-06-08 14:31:21 +0300

    Added database (db) header

---
+++ include/db.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <sqlite3.h>
+#include <stddef.h>
+
+int db_init(const char *db_path);
+
+int table_insert(const char *short_code, const char *original_url);
+
+int table_lookup(const char *short_code, char *output_url, size_t output_size);
+
+void db_close();
