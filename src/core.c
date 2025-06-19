9611e5c005db3634ebc34ebb8c993dd745c1977d
n4tas <natas.mazeika@gmail.com>
2025-06-19 13:21:46 +0300

    Add initial http_server.h and http_server.c files

--- src/core.c
+++ src/core.c
@@ -1,16 +1,5 @@
 #include "../include/core.h"
 
-int handleAdd(const char *url) 
-{
-	unsigned char hash[SHA256_DIGEST_LENGTH];
-	char short_code[SHORT_CODE_LENGTH];
-	char salted_string[SALTED_STRING_SIZE] = {0};
-	if (dbInit(URL_PATH) != SQLITE_OK) return SQLITE_ERROR;
-	if (finalHashing(salted_string, url, hash, short_code) != 0) return HASHING_ERR_NULL_INPUT;
-	if (tableAdd(short_code, url) != 0) return SQLITE_ERROR;
-	return 0;
-}
-
 int handleResolve(const char *short_code)
 {
 	int rc = dbInit(URL_PATH);
