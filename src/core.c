540dd42002a6849807878bccbcf1d1f458377874
n4tas <natas.mazeika@gmail.com>
2025-06-19 13:00:20 +0300

    Fix missing error code definitions in handleAdd
    - Replaced undeclared error macros with appropriate SQLite and hashing return codes
    - Updated handleAdd to return defined constants like SQLITE_ERROR and HASHING_ERR_NULL_INPUT
    - Ensured all error paths are handled using standard codes

--- src/core.c
+++ src/core.c
@@ -5,9 +5,9 @@ int handleAdd(const char *url)
 	unsigned char hash[SHA256_DIGEST_LENGTH];
 	char short_code[SHORT_CODE_LENGTH];
 	char salted_string[SALTED_STRING_SIZE] = {0};
-	if (dbInit(URL_PATH) != 0) return ERR_DB_INIT;
+	if (dbInit(URL_PATH) != SQLITE_OK) return SQLITE_ERROR;
 	if (finalHashing(salted_string, url, hash, short_code) != 0) return HASHING_ERR_NULL_INPUT;
-	if (tableAdd(short_code, url) != 0) return ERR_DB_PROCESS;
+	if (tableAdd(short_code, url) != 0) return SQLITE_ERROR;
 	return 0;
 }
 
